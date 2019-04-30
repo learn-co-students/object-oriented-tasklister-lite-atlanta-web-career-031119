d1dd9683c44c7b549dc62dea0fddf30c5311042d
PrincetonJRose <decoyzpr@gmail.com>
2019-04-30 09:19:10 -0400

    Got list working and able to display. Added remove button that removes list item in the DOM and the array. Working on finishing the priority selector.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+let id = 0
+let newTaskList = new TaskList()
 
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+document.addEventListener("DOMContentLoaded", () => {
+  // const taskList = new TaskList();
+
+  // const listContainer = document.getElementById("list");
+  // const renderApp = () => (listContainer.innerHTML = taskList.render());
 
   // Add Form Behavior Here!
+  document.getElementById('create-task-form').addEventListener("submit", createTask)
 
-  renderApp()
+  // renderApp()
 });
+
+function createTask(e){
+  e.preventDefault()
+  let newTask = new TaskListItem(e.target.newtaskdescription.value, id++)
+  e.target.reset()
+  newTaskList.tasks.push(newTask)
+  newTaskList.appendUl()
+  
+}
+
+function removeTask(e){
+  console.log(e.target)
+  for (let index = 0; index < newTaskList.tasks.length; index++){
+    if (newTaskList.tasks[index].id == e.target.dataset.id){
+      newTaskList.tasks.splice(index, 1)
+    }
+  }
+
+  e.target.parentElement.remove()
+}
